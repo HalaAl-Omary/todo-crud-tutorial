0a4313a87017858d84e834468af08a9764284a5c
HalaAl-Omary <omaryhala35@gmail.com>
2025-06-09 23:26:51 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,78 @@
+const from = document.getElementById("add-form");
+const addinput = document.querySelector("#add-input");
+const Todos = JSON.parse(localStorage.getItem("todos")) || [];
+function addtodo(e) {
+  e.preventDefault();
+  const addInputValue = addinput.value.trim();
+  if (addInputValue.length === 0) {
+    addinput.nextElementSibling.innerText = "*This field is required!";
+    return;
+  }
+  addinput.nextElementSibling.innerText = " ";
+  const newTodo = {
+    todo: addInputValue,
+    completed: false,
+    isUpdating: false,
+  };
+
+  Todos.push(newTodo);
+  localStorage.setItem("todos", JSON.stringify(Todos));
+  from.reset();
+  console.log(Todos);
+  displayTodos();
+}
+function displayTodos() {
+  let result = " ";
+  Todos.forEach((item, index) => {
+    result += `
+        <tr>
+        <td>${index + 1} </d>
+        <td>${item.isUpdating ? `<input type="text" data-index="${index}" value="${item.todo}"/>` : item.todo}</td>
+        <td><input onclick="toggleCompleted(${index})" type="checkbox" ${item.completed ? 'checked' : ""}  /></td>
+        <td>
+          ${item.isUpdating ? ` <button class="btn btn-success"
+            onclick="saveTodo(${index})">Save</button>`
+        :
+        `<button class="btn btn-primary" onclick="setUpdating(${index})">Update</button>`}
+            
+<button onclick="deletetodo(${index})" class="btn btn-danger ">Delete</button> 
+        </td>
+          </tr>
+          `;
+  });
+  document.getElementById("todos").innerHTML = result;
+}
+
+function toggleCompleted(indx) {
+  Todos[indx].completed = !Todos[indx].completed;
+  console.log(Todos);
+  localStorage.setItem("todos", JSON.stringify(Todos));
+  displayTodos();
+}
+
+function deletetodo(index) {
+  Todos.splice(index, 1);
+  localStorage.setItem("todos", JSON.stringify(Todos));
+  displayTodos();
+}
+
+function setUpdating(index) {
+  Todos[index].isUpdating = true;
+  localStorage.setItem("todos", JSON.stringify(Todos));
+  displayTodos();
+}
+
+
+function saveTodo(index){
+const input = document.querySelector(`input[data-index ="${index}"]`)
+console.log(input, input.value);
+Todos[index].todo = input.value.trim()
+Todos[index].isUpdating = false;
+localStorage.setItem('todos' , JSON.stringify(Todos))
+displayTodos();
+
+}
+
+displayTodos();
+
+from.addEventListener("submit", addtodo);
